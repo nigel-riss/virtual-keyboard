4b3be652a452c55a706f1ddcf275b8f89afcf2b0
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2023-05-01 06:16:50 +0200

    feat: add proper cursor and selection handling for textarea

--- src/script/textarea.js
+++ src/script/textarea.js
@@ -7,6 +7,7 @@ class Textarea {
     this.textarea.className = 'textarea';
     this.textarea.rows = 6;
     this.value = '';
+    this.cursorPosition = 0;
 
     this.parentElement.append(this.textarea);
 
@@ -17,15 +18,30 @@ class Textarea {
     document.addEventListener('vKeyPress', (e) => {
       console.log('Got event', e.value);
       if (!KEYS[e.code].isControlKey) {
-        this.value += e.value;
+        this.insertText(e.value);
+        this.cursorPosition += 1;
+        this.render();
       }
-      this.render();
     });
+
+    this.textarea.addEventListener('click', () => {
+      this.cursorPosition = this.textarea.selectionStart;
+      console.log(this.cursorPosition);
+    });
+  }
+
+  insertText(text) {
+    // const before = this.value.slice(0, this.cursorPosition);
+    // const after = this.value.slice(this.cursorPosition, this.value.length);
+    const before = this.value.slice(0, this.textarea.selectionStart);
+    const after = this.value.slice(this.textarea.selectionEnd, this.value.length);
+    this.value = before + text + after;
   }
 
   render() {
     this.textarea.innerText = this.value;
     this.textarea.focus();
+    this.textarea.selectionStart = this.cursorPosition;
   }
 }
 
